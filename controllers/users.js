6582519afbf96006e33c09c1c22719840754bf2b
brogers-97 <brburr24@gmail.com>
2023-04-06 12:28:56 -0700

    adds server and leaves route pseucode

---
+++ controllers/users.js
@@ -0,0 +1,7 @@
+// required packages
+const express = require('express')
+const router = express.Router()
+//mount routes
+
+//export the router instance
+module.exports = router
---
+++ index.js
@@ -0,0 +1,18 @@
+// required packages
+require('dotenv').config()
+const express = require('express')
+// app config
+const app = express()
+const PORT = process.env.PORT || 8000
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({extended:false}))
+
+app.get('/', (req, res) => {
+    res.send('welcome home Brian')
+})
+
+app.use('/users', require('./controllers/users.js'))
+
+app.listen(PORT, function(){
+    console.log(`server ${PORT} linked!`)
+})
